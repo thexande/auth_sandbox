80e1cf772d394607e755e7066ed1a5f8719228c9
Nmuta Jones <nmuta1@gmail.com>
2016-02-17 14:30:17 -0700

    many to many relationship for teams

--- routes/employees.js
+++ routes/employees.js
@@ -7,9 +7,40 @@ function Employees() {
 }
 
 router.get('/', function(req, res, next) {
-    console.log("reached employees");
-    Employees().select().then(function (employees) {
-      res.render('employees/index', {employees: employees});
+      var line1 = "SELECT employees.name , teams.team_name FROM employees ";
+      var line2 = "join assignments on employees.id = assignments.employee_id ";
+      var line3 = "join teams on teams.id = assignments.team_id " ;
+      var line4 = line1+line2+line3;
+
+      var people = [];
+
+      knex.raw(line4)
+      .then(function (employees) {
+      console.log(employees);
+      employees = employees.rows;
+
+      employees.forEach(function(employee){
+        var in_array = people.filter(function(thing){
+            return thing.name === employee.name;
+        });
+
+        if (in_array.length === 0){
+           var team_array = [];
+           team_array.push(employee.team_name);
+           people.push({name: employee.name, teams: team_array})
+        } else {
+          people.forEach(function (person){
+            if (person.name === employee.name){
+              person.teams.push(employee.team_name);
+            }
+          });
+        }
+
+
+      });
+
+      console.log("my people are ", people);
+      res.render('employees/index', {employees: employees, people: people});
     });
 });
 
